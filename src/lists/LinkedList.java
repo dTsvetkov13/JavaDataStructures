c83d04629070c2d643a31bbce932f415cc15e387
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-03-06 17:09:10 +0200

    Add method implemented

--- src/lists/LinkedList.java
+++ src/lists/LinkedList.java
@@ -1,27 +1,58 @@
 package lists;
 
 import interfaces.IList;
+import models.Node;
 
 public class LinkedList<T> implements IList<T>
 {
-
+	private Node<T> first;
+	
 	@Override
 	public boolean isEmpty()
 	{
+		if(first == null)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean contains(T item)
+	{
+		Node<T> node = this.first;
+		
+		while(node != null)
+		{
+			if(node.getValue() == item)
+			{
+				return true;
+			}
+			
+			node = node.getNext();
+		}
 		
 		return false;
 	}
 
 	@Override
-	public boolean contains(T item) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void add(T item) {
-		// TODO Auto-generated method stub
+	public void add(T item)
+	{
+		Node<T> node = this.first;
 		
+		while(node != null)
+		{
+			if(!node.hasNext())
+			{
+				Node temp = new Node(item);
+				temp.setPrevious(node);
+				
+				node.setNext(temp);
+				return;
+			}
+			
+			node = node.getNext();
+		}
 	}
 
 	@Override
