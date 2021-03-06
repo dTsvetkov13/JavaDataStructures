da9c55e9e85a5519809555778776725eb474d7ca
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-03-06 16:34:52 +0200

    init

---
+++ src/interfaces/IList.java
@@ -0,0 +1,20 @@
+package interfaces;
+
+public interface IList<T>
+{
+	boolean isEmpty(); 
+
+	boolean contains(T item); 
+
+	void add(T item);
+
+	void insertAt(T item, int index); 
+
+	void removeFrom(int index);
+
+	void remove(T item);
+
+	T get(int index);
+
+	void clear();
+}
