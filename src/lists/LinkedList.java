d5b008b8b9927b451658da2e54a2209963920c6c
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-03-06 17:02:25 +0200

    init

---
+++ src/lists/LinkedList.java
@@ -0,0 +1,57 @@
+package lists;
+
+import interfaces.IList;
+
+public class LinkedList<T> implements IList<T>
+{
+
+	@Override
+	public boolean isEmpty()
+	{
+		
+		return false;
+	}
+
+	@Override
+	public boolean contains(T item) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void add(T item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insertAt(T item, int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeFrom(int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(T item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
