10aad603e47e8c75968b1cf2884d7ef6e0165d7b
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-03-11 14:03:18 +0200

    Implement add() and get() and calculate some complexities

--- src/lists/SortedList.java
+++ src/lists/SortedList.java
@@ -1,11 +1,12 @@
 package lists;
 
 import interfaces.IList;
+import models.DoublyNode;
 import models.Node;
 
 public class SortedList<T extends Comparable<T>> implements IList<T>
 {
-	private Node<T> first;
+	private DoublyNode<T> first;
 	private int count;
 	
 	public SortedList()
@@ -20,12 +21,13 @@ public class SortedList<T extends Comparable<T>> implements IList<T>
 	}
 
 	// O(n) complexity
+	//implement with binary search
 	@Override
 	public boolean contains(T item)
 	{
 		if(this.first == null) return false;
 		
-		Node<T> currentNode = this.first;
+		DoublyNode<T> currentNode = this.first;
 		
 		while(currentNode != null)
 		{
@@ -40,9 +42,56 @@ public class SortedList<T extends Comparable<T>> implements IList<T>
 		return false;
 	}
 
+	// O(n) complexity
 	@Override
 	public void add(T item)
 	{
+		if(this.first == null)
+		{
+			this.first = new DoublyNode<T>(item);
+			this.count++;
+			return;
+		}
+		
+		DoublyNode<T> currentNode = this.first;
+		
+		while(currentNode != null && 
+			  currentNode.getValue().compareTo(item) < 0)
+		{
+			if(currentNode.hasNext())
+			{
+				currentNode = currentNode.getNext();	
+			}
+			else break;
+		}
+		
+		if(!currentNode.hasNext())
+		{
+			DoublyNode<T> toAdd = new DoublyNode<T>(item);
+			currentNode.setNext(toAdd);
+			toAdd.setPrevious(currentNode);
+			this.count++;
+			return;
+		}
+		
+		if(this.first.equals(currentNode))
+		{
+			DoublyNode<T> toAdd = new DoublyNode<T>(item);
+			toAdd.setNext(currentNode);
+			currentNode.setPrevious(toAdd);
+			
+			this.first = toAdd;
+			this.count++;
+			return;
+		}
+		
+		DoublyNode<T> toAdd = new DoublyNode<T>(item);
+		toAdd.setNext(currentNode);
+		toAdd.setPrevious(currentNode.getPrevious());
+		
+		currentNode.getPrevious().setNext(toAdd);
+		currentNode.setPrevious(toAdd);
+		this.count++;
 	}
 
 	@Override
@@ -59,20 +108,44 @@ public class SortedList<T extends Comparable<T>> implements IList<T>
 	public void remove(T item)
 	{	
 	}
-
+	
+	// O(n) complexity
 	@Override
 	public T get(int index)
 	{
-		return null;
+		if(this.first == null) throw new IllegalArgumentException("Invalid index");
+		
+		DoublyNode<T> currentNode = this.first;
+		int i = 0;
+		
+		while(currentNode != null)
+		{
+			if(i == index)
+			{
+				break;
+			}
+			
+			currentNode = currentNode.getNext();
+			i++;
+		}
+		
+		if(i < index)
+		{
+			throw new IllegalArgumentException("Invalid index");
+		}
+		
+		return currentNode.getValue();
 	}
 
+	// O(1) complexity
 	@Override
 	public void clear()
 	{
 		this.first = null;
 		this.count = 0;
 	}
-
+	
+	// O(1) complexity
 	@Override
 	public int size()
 	{
